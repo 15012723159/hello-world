2ff283d8e17bf2d7bd963f434de5611c5ae4709a
qinfuxing <qinfuxing@anycubic.com>
2025-02-28 17:27:54 +0800

    first commit

---
+++ src/HelloWorld.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @Notes:
+ * @Author: qinfuxing
+ * @Date: 2025/2/28
+ * @Time: 17:27
+ * @Interface HelloWorld
+ * @return
+ */
+
+/**
+ * User: qinfuxing
+ * Date: 2025/2/28
+ * Time: 17:27
+ */
+class HelloWorld
+{
+    public static function sayHello()
+    {
+        return "Hello, World!";
+    }
+}
